784cd40fb5d179882b574755845262010803b2fa
David Chang <chechiachang999@gmail.com>
2015-09-24 15:30:31 +0800

    Stuck

--- src/main/java/com/ccc/nhr/server/CoordinatorDataService.java
+++ src/main/java/com/ccc/nhr/server/CoordinatorDataService.java
@@ -30,7 +30,7 @@ public class CoordinatorDataService {
         this.dataInputStream = new DataInputStream(socket.getInputStream());
     }
 
-    boolean isPrintout = false;
+    boolean isPrintout = false; //set true to debug
 
     String s = null;
     String[] scanner = {"0", "0", "0", "0"};
@@ -47,7 +47,7 @@ public class CoordinatorDataService {
     PreparedStatement pstmt = null;
     ResultSet rs;
 
-    public void getRequest() throws IOException {
+    public void getRequest() throws IOException {   //test method
         String str = null;
         System.out.print(new Date() + " -> ");
         while ((str = Integer.toHexString(dataInputStream.read())) != null) {
@@ -81,15 +81,20 @@ public class CoordinatorDataService {
                     System.out.print(new Date() + " -> ");
                 }
 
-                //head scanner
+                /*
+                 * head scanner
+                 * each input byte sequently put into scanner
+                 * if scanner get 'A' 'T' '+'
+                 * next byte will be total packet length
+                 */
                 scanner[0] = scanner[1];
                 scanner[1] = scanner[2];
                 scanner[2] = scanner[3];
-                scanner[3] = s;
+                scanner[3] = s; //get data length(hex)
 
                 //refresh dataLength
                 if (scanner[0].equals("41") && scanner[1].equals("54") && scanner[2].equals("2b")) {
-                    dataLength = Integer.parseInt(s, 16);
+                    dataLength = Integer.parseInt(s, 16);   //parse data length to decimal
                     count = 3;
                     output = new String[dataLength + 3];
                     output[0] = "41";
@@ -97,166 +102,72 @@ public class CoordinatorDataService {
                     output[2] = "2b";
                 }
 
-                //output
-                if (s.length() == 1) {
+                if (s.length() == 1) {  // if packet has less than 10 bytes, add "0" to data length byte
                     s = "0" + s;
                 }
                 output[count] = s;
-            //System.out.print(s + " ");
-
-                /*
-                 //if read head
-                 if (count < 4) {
-
-                 } else //if finished read frame type
-                 if (count == 4) {
-
-                 } else //if finished read device IEEE address
-                 if (count == 12) {
-
-                 } else //if finished read device short address
-                 if (count == 14) {
-
-                 } else //if finished read Source Endpoint
-                 if (count == 15) {
-
-                 } else //if finished read Destination Endpoint
-                 if (count == 16) {
-
-                 } else //if finished read Cluster ID
-                 if (count == 18) {
-
-                 } else //if finished read Profile ID
-                 if (count == 20) {
-
-                 } else //if finished read Recieve options
-                 if (count == 21) {
-
-                 } else //if finished read Frame Control byte
-                 if (count == 22) {
-
-                 } else //if finished read Transit sequence number
-                 if (count == 23) {
-
-                 } else //if finished read ZCL command
-                 if (count == 24) {
-
-                 } else //if finished read Attribute ID
-                 if (count == 26) {
-
-                 } else //if finished read data type
-                 if (count == 27) {
-
-                 } else //if finished read value
-                 if (count == 29) {
-
-                 } else //if finished read check byte
-                 if (count == 30) {
-                
-                 }
-                 */
-                //if at the end of data
-                if (end = (count == dataLength)) {
-                    String out = "";
+                //System.out.print(s + " ");
+                if (end = (count == dataLength)) {  //at the end of data, println output
+                    String out;
                     System.out.println(Arrays.toString(output));
 
-                    //if is devices data 
-                    if (output.length > 26) {
-                        //switch Cluster ID
-                        switch (output[17]) {
-                            case "00":
-                                switch (output[18]) {
-                                    case "01":  //ZigBee Cluster Library power configuration cluster ID
+                    //Save data into database
+                    if(output.length > 35){   // Cluster Library device power status reply, length = 0x21 = 33, +2 null = 35
+                        
+                    }else if (output.length > 31) {   // Cluster Library device reply data, length = 0x1e = 30, +2 null = 32
+                        String data = String.valueOf(Integer.parseInt(output[29] + output[28], 16));
+                        String clusterId = "0405";
+                        switch (output[17] + output[18]) {  //switch Cluster ID
+                            case "0001":    //ZigBee Cluster Library power configuration cluster ID
+                                break;
+                            case "0405":    //0405 ZigBee Cluster Library relative humidity measurement cluster ID
+                                clusterId = "0405";
+                                break;
+                            case "0402":    //ZigBee Cluster Library relative humidity measurement cluster ID 0x0405
+                                clusterId = "0402";
+                                break;
+                        }
+                        pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
+                        pstmt.setString(1, returnOutput(5, 12) + "0405");
+                        pstmt.setString(2, returnOutput(13, 14));
+                        pstmt.setString(3, clusterId);
+                        pstmt.setString(4, data.substring(0, 2) + "." + data.substring(2, 4));
+                        pstmt.executeUpdate();
+                        if (isPrintout) {
+                            out = "Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0405" + " Data: " + data.substring(0, 2) + "." + data.substring(2, 4) + " %";
+                            System.out.println(out);
+                        }
+                    } else if (output.length > 28) {    //data , length = 0x1e = 30, +2 null = 32
+                        String IASCmd = "";
+                        switch (output[17] + output[18]) {
+                            case "0500":    //0500 (ZCL_CLUSTER_ID_SS_IAS_ZONE)
+                                switch (output[26]) {
+                                    case "03":  //on
+                                        IASCmd = "on";
+                                        break;
+                                    case "00":  //off
+                                        IASCmd = "off";
+                                        break;
+                                    case "08":  //on & low battery
+                                        IASCmd = "on/low";
+                                        break;
+                                    case "0c":  //off & low battery
+                                        IASCmd = "off/low";
                                         break;
                                 }
-                                break;
-                            case "04":
-                                switch (output[18]) {
-                                    case "05":  //0405 ZigBee Cluster Library relative humidity measurement cluster ID
-                                        String humid = String.valueOf(Integer.parseInt(output[29] + output[28], 16));
-
-                                        pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
-                                        pstmt.setString(1, returnOutput(5, 12) + "0405");
-                                        pstmt.setString(2, returnOutput(13, 14));
-                                        pstmt.setString(3, "0405");
-                                        pstmt.setString(4, humid.substring(0, 2) + "." + humid.substring(2, 4));
-                                        pstmt.executeUpdate();
-
-                                        if (isPrintout) {
-                                            out = "Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0405" + " Data: " + humid.substring(0, 2) + "." + humid.substring(2, 4) + " %";
-                                            System.out.println(out);
-                                        }
-                                        break;
-                                    case "02":  //ZigBee Cluster Library relative humidity measurement cluster ID 0x0405
-                                        String temp = String.valueOf(Integer.parseInt(output[29] + output[28], 16));
-
-                                        pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
-                                        pstmt.setString(1, returnOutput(5, 12) + "0402");
-                                        pstmt.setString(2, returnOutput(13, 14));
-                                        pstmt.setString(3, "0402");
-                                        pstmt.setString(4, temp.substring(0, 2) + "." + temp.substring(2, 4));
-                                        pstmt.executeUpdate();
-
-                                        if (isPrintout) {
-                                            out = "Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0402" + " Data: " + temp.substring(0, 2) + "." + temp.substring(2, 4) + " Celcius";
-                                            System.out.println(out);
-                                        }
-                                        break;
-                                }
-                                break;
-                            case "05":
-                                switch (output[18]) {
-                                    case "00":  //0500 (ZCL_CLUSTER_ID_SS_IAS_ZONE)
-                                        switch (output[26]) {
-                                            case "03":
-                                                pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
-                                                pstmt.setString(1, returnOutput(5, 12) + "0500");
-                                                pstmt.setString(2, returnOutput(13, 14));
-                                                pstmt.setString(3, "0500");
-                                                pstmt.setString(4, "on");
-                                                pstmt.executeUpdate();
-                                                if (isPrintout) {
-                                                    System.out.println("Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0500" + " Data: " + "on");
-                                                }
-                                                break;
-                                            case "00":
-                                                pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
-                                                pstmt.setString(1, returnOutput(5, 12) + "0500");
-                                                pstmt.setString(2, returnOutput(13, 14));
-                                                pstmt.setString(3, "0500");
-                                                pstmt.setString(4, "off");
-                                                pstmt.executeUpdate();
-                                                if (isPrintout) {
-                                                    System.out.println("Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0500" + " Data: " + "off");
-                                                }
-                                                break;
-                                            case "08":
-                                                pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
-                                                pstmt.setString(1, returnOutput(5, 12) + "0500");
-                                                pstmt.setString(2, returnOutput(13, 14));
-                                                pstmt.setString(3, "0500");
-                                                pstmt.setString(4, "on/low");
-                                                pstmt.executeUpdate();
-                                                if (isPrintout) {
-                                                    System.out.println("Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0500" + " Data: " + "on / low battery");
-                                                }
-                                                break;
-                                            case "0c":
-                                                pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
-                                                pstmt.setString(1, returnOutput(5, 12) + "0500");
-                                                pstmt.setString(2, returnOutput(13, 14));
-                                                pstmt.setString(3, "0500");
-                                                pstmt.setString(4, "off/low");
-                                                pstmt.executeUpdate();
-                                                if (isPrintout) {
-                                                    System.out.println("Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0500" + " Data: " + "off / low battery");
-                                                }
-                                                break;
-                                        }
-                                        break;
+                                pstmt = conn.prepareStatement("INSERT INTO `data` (`mac_cluster_id`, `short_mac`, `cluster_id`, `data`) VALUES (?,?,?,?) ON DUPLICATE KEY UPDATE data=VALUES(data)");
+                                pstmt.setString(1, returnOutput(5, 12) + "0500");
+                                pstmt.setString(2, returnOutput(13, 14));
+                                pstmt.setString(3, "0500");
+                                pstmt.setString(4, IASCmd);
+                                pstmt.executeUpdate();
+                                if (isPrintout) {
+                                    System.out.println("Mac Address: " + returnOutput(5, 12) + " Device Short Mac: " + returnOutput(13, 14) + " Cluster ID: " + "0500" + " Data: " + "on");
                                 }
                                 break;
                         }
+                    }else if(output.length > 21){   // Local Command query reply packet [19+2]
+                        
                     }
                     count = 0;
 
